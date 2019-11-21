81555fa915ee3593d4f396e079ea4c41f65b50ea
Shestakova Sofya <soneshya@mail.ru>
2019-11-21 21:42:00 +0300

    Add dynamic panels.

--- func.js
+++ func.js
@@ -1,19 +1,13 @@
-function showOrHidePanel(element1){
-var panel  =  document.getElementById(element1);
-if (this.panel == null) {
-   var b =   document.createElement(element1);
-   panel =document.getElementById(element1);
-    console.log("AAAAAAAAAAAAAAAA NOT DEFIEMED"+panel);
-}
-console.log("THOIS IS PANEL ELEMENHT"+panel);
- var style = panel.style;
-    if (!(panel.style.display = "block")) {
-        panel.style.display = "block";
+function showOrHidePanel(element1) {
+    var panel = document.getElementById(element1);
+    var rightPanel = document.getElementById('right-panel');
+    if (panel.style.display !== 'block') {
+        panel.style.display = 'block';
     } else {
-        panel.style.display = "none";
+        panel.style.display = 'none';
     }
-
 }
+
 
 
 
